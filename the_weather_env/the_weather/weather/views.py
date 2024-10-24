60a1a592a7f952cb531702a5e9de300e65432b45
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-10-24 10:28:47 +0100

    upgrade

--- the_weather_env/the_weather/weather/views.py
+++ the_weather_env/the_weather/weather/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-import requests
+import requests, environ
 from .models import City
 from .forms import CityForm
+
+env = environ.Env()
+
+environ.Env.read_env()
+
+API_KEY = env("API_KEY")
 
 
 # Create your views here.
 def index(request):
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=YOUR_APP_KEY'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format('{}', API_KEY)
 
     cities = City.objects.all()  # return all cities in the database
 
@@ -19,8 +25,7 @@ def index(request):
     weather_data = []
 
     for city in cities:
-        city_weather = requests.get(
-            url.format(city)).json()  # request the API data and convert the JSON to Python data types
+        city_weather = requests.get(url.format(city)).json()  # request the API data and convert the JSON to Python data types
 
         weather = {
             'city': city,
@@ -28,7 +33,6 @@ def index(request):
             'description': city_weather['weather'][0]['description'],
             'icon': city_weather['weather'][0]['icon']
         }
-        print(weather)
 
         weather_data.append(weather)  # add the data for the current city into the list
 
